2cfcb94efa0a96c00e5995d62b093c65036e5250
小莱昂纳德 <66188911+Leonard-Jeffrey@users.noreply.github.com>
2022-03-01 11:35:20 +0800

    Update InsertSort.cpp

--- C/Sort/InsertSort.cpp
+++ C/Sort/InsertSort.cpp
@@ -1,18 +1,18 @@
 #include"sort.h"
 
 int* InsertSort(int d[], int len) {
-	int i, j, key;
 	//int len = sizeof(d) / sizeof(d[0]);
 	if (len < 2)
 		return d;
-	for (i = 1; i < len; i++) {
+	for (int i = 1; i < len; i++) {
 		//Find
-		int temp = d[i];
-		for (key = i - 1;key >= 0 && d[key] > temp;key--) {
-			d[key+1] = d[key];
+		int flag = d[i];
+		int j;
+		for (j = i - 1;j >= 0 && d[j] > temp;j--) {
+			d[j+1] = d[j];
 		}
-		//Store(d[key+1], d[i])
-		d[key + 1] = temp;
+		//Store(d[j+1], d[i])
+		d[j + 1] = temp;
 	}
 	return d;
 }
